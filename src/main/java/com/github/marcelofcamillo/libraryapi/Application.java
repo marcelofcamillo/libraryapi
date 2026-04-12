134ff28d4fe7951befb506ed548172a3be2dc72f
Marcelo Camillo <marcelocamillo18@gmail.com>
2026-04-11 23:32:52 -0300

    Executando uma operação de persistência com repositório

--- src/main/java/com/github/marcelofcamillo/libraryapi/Application.java
+++ src/main/java/com/github/marcelofcamillo/libraryapi/Application.java
@@ -9,7 +9,20 @@ import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		var context = SpringApplication.run(Application.class, args);
+		AutorRepository repository = context.getBean(AutorRepository.class);
+
+		exemploSalvarRegistro(repository);
+	}
+
+	public static void exemploSalvarRegistro(AutorRepository autorRepository) {
+		Autor autor = new Autor();
+		autor.setNome("José");
+		autor.setNacionalidade("Brasileira");
+		autor.setDataNascimento(LocalDate.of(1950, 1, 31));
+
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("Autor salvo: " + autorSalvo);
+	}
 }
